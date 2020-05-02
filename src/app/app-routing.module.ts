4bf5953fe8144b025ae94ba58310a29f52c5566a
Bojana Dimovikj <bojana.dimovik@live.com>
2020-05-03 00:09:31 +0200

    Added animations on all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './components/pages/home/home.component';
+import { MyAccountComponent } from './components/pages/my-account/my-account.component';
+import { CompaniesComponent } from './components/pages/companies/companies.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
-import { MyAccountComponent } from './components/pages/my-account/my-account.component';
 import { AdminPanelComponent } from './components/pages/admin-panel/admin-panel.component';
-
-import { AuthGuardGuard } from './shared/auth-guard.guard';
-import { CompaniesComponent } from './components/pages/companies/companies.component';
 import { TermsOfServiceComponent } from './components/pages/terms-of-service/terms-of-service.component';
+import { AuthGuardGuard } from './shared/auth-guard.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { animationState: 'Home' } },
+  { path: '', 
+    component: HomeComponent, 
+    data: { animationState: 'Home' } 
+  },
+  {
+    path: 'my-account',
+    component: MyAccountComponent,
+    data: { animationState: 'MyAccount' },
+    canActivate: [AuthGuardGuard]
+  },
+  {
+    path: 'companies',
+    component: CompaniesComponent,
+    data: { animationState: 'Companies' }
+  },
   {
     path: 'about',
     component: AboutComponent,
@@ -23,26 +37,15 @@ const routes: Routes = [
     data: { animationState: 'Contact' }
   },
   {
-    path: 'my-account',
-    component: MyAccountComponent,
-    data: { animationState: 'About' },
-    canActivate: [AuthGuardGuard]
-  },
-  {
-    path: 'admin',
+    path: 'admin-panel',
     component: AdminPanelComponent,
-    data: { animationState: 'Contact' },
+    data: { animationState: 'Admin' },
     canActivate: [AuthGuardGuard]
-  },
-  {
-    path: 'companies',
-    component: CompaniesComponent,
-    data: { animationState: 'Contact' }
   },
   {
     path: 'terms-of-service',
     component: TermsOfServiceComponent,
-    data: { animationState: 'About' }
+    data: { animationState: 'TermsOfService' }
   }
 ];
 
