9ea15848cb18242e06072f8050942a0ee8ed3f23
Antonij Dzajkoski <djajkoski@its-sk.com.mk>
2020-05-08 16:06:02 +0200

    new res logo and favicon, about changed

--- src/app/components/reviews/votes-container/votes-container.component.ts
+++ src/app/components/reviews/votes-container/votes-container.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../../shared/auth.service';
 @Component({
   selector: 'app-votes-container',
   templateUrl: './votes-container.component.html',
-  styleUrls: ['./votes-container.component.scss'],
+  styleUrls: ['./votes-container.component.scss']
 })
 export class VotesContainerComponent implements OnInit {
   @Input() id: string; // ReviewID
@@ -23,27 +23,27 @@ export class VotesContainerComponent implements OnInit {
   }
 
   renderUpvotes() {
-    this.rs.getUpvotes(this.id).subscribe((e) => {
-      this.upvoteList = e.map((item) => item.payload.doc.data());
-      this.as.afAuth.user.subscribe((user) => {
-        this.loggedUser = user.email;
+    this.rs.getUpvotes(this.id).subscribe(e => {
+      this.upvoteList = e.map(item => item.payload.doc.data());
+      this.as.afAuth.user.subscribe(user => {
+        this.loggedUser = user?.email;
         this.controlButton(this.loggedUser);
       });
     });
   }
 
   renderDownvotes() {
-    this.rs.getDownvotes(this.id).subscribe((e) => {
-      this.downvoteList = e.map((item) => item.payload.doc.data());
-      this.as.afAuth.user.subscribe((user) => {
-        this.loggedUser = user.email;
+    this.rs.getDownvotes(this.id).subscribe(e => {
+      this.downvoteList = e.map(item => item.payload.doc.data());
+      this.as.afAuth.user.subscribe(user => {
+        this.loggedUser = user?.email;
         this.controlButton(this.loggedUser);
       });
     });
   }
 
   upvoteBtn() {
-    this.as.getUsername().subscribe((e) => {
+    this.as.getUsername().subscribe(e => {
       this.rs.upvoteReview(this.id, 1, e.email);
       this.renderUpvotes();
       this.controlButton(this.loggedUser);
@@ -51,7 +51,7 @@ export class VotesContainerComponent implements OnInit {
   }
 
   downvoteBtn() {
-    this.as.getUsername().subscribe((e) => {
+    this.as.getUsername().subscribe(e => {
       this.rs.upvoteReview(this.id, -1, e.email);
       this.renderDownvotes();
       this.controlButton(this.loggedUser);
@@ -59,13 +59,13 @@ export class VotesContainerComponent implements OnInit {
   }
 
   controlButton(mail) {
-    this.upvoteList.forEach((item) => {
+    this.upvoteList.forEach(item => {
       if (item.username === mail) {
         this.upvoteBlocked = true;
         this.downvoteBlocked = true;
       }
     });
-    this.downvoteList.forEach((item) => {
+    this.downvoteList.forEach(item => {
       if (item.username === mail) {
         this.upvoteBlocked = true;
         this.downvoteBlocked = true;
