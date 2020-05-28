29bd47d9e2e1aa97dfb588c879c80d2d0bab80f5
Antonij Dzajkoski <djajkoski@its-sk.com.mk>
2020-05-28 20:05:01 +0200

    Company full page init setup

--- src/app/components/pages/companies/company-full-page/company-full-page.component.ts
+++ src/app/components/pages/companies/company-full-page/company-full-page.component.ts
@@ -3,6 +3,7 @@ import { CompanyService } from 'src/app/shared/company.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { ReviewService } from 'src/app/shared/review.service';
 
 @Component({
   selector: 'app-company-full-page',
@@ -12,12 +13,15 @@ import { ToastrService } from 'ngx-toastr';
 export class CompanyFullPageComponent implements OnInit {
   currentCompany: any = {};
   currentCompanyName = '';
+  reviews = [];
+  canLoadReviews = false;
 
   constructor(
     private companyService: CompanyService,
     private router: Router,
     private auth: AuthService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private reviewService: ReviewService
   ) {
     this.currentCompanyName = router.url.slice(11);
   }
@@ -32,13 +36,30 @@ export class CompanyFullPageComponent implements OnInit {
           return;
         }
         res.forEach(element => {
-          console.log(element.payload.doc.data());
           this.currentCompany = element.payload.doc.data();
         });
       },
       errorRes => {
         this.auth.showHTTPLoader(false);
         this.toastr.error(errorRes.message);
+      }
+    );
+  }
+
+  loadReviews() {
+    this.auth.showHTTPLoader(true);
+    this.reviewService.getReviewsForCompany(this.currentCompanyName).subscribe(
+      res => {
+        this.canLoadReviews = true;
+
+        this.auth.showHTTPLoader(false);
+        res.forEach(element => {
+          this.reviews.push(element.payload.doc.data());
+        });
+      },
+      err => {
+        this.auth.showHTTPLoader(false);
+        this.toastr.error(err.message);
       }
     );
   }
