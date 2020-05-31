aadcfe05b9b740bddc0f66214f4c704706ebb2d9
bazejbt <bazejbt@yahoo.co.uk>
2020-05-31 14:31:08 +0200

    desingTerms of Service - Redesign

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -15,12 +15,15 @@ export const light: Theme = {
     '--shadowsOpacified': 'rgba(0, 0, 0, 0.8)',
     '--outlines': '#ddddddd2',
     '--font-color': 'whitesmoke',
-    '--reverse-font-color': 'black',
+    '--reverse-font-color': '#212121',
     '--gradient-one': 'rgba(52, 88, 128, 1)',
     '--gradient-one-faded': '#516488',
     '--gradient-two': 'rgba(48, 51, 65, 1)',
     '--opacity1': 'none',
-    '--opacity2': '0.4'
+    '--opacity2': '0.4',
+    '--tos-border': '#1d2141',
+    '--tos-bg-color': '#2E2D2D',
+    '--test': '#1d2141',
   }
 };
 
@@ -35,12 +38,15 @@ export const dark: Theme = {
     '--shadows': 'rgba(0, 0, 0, 0.4)',
     '--shadowsOpacified': 'rgba(0, 0, 0, 0.8)',
     '--outlines': '#ddddddd2',
-    '--font-color': 'whitesmoke',
-    '--reverse-font-color': 'black',
+    '--font-color': 'black',
+    '--reverse-font-color': 'whitesmoke',
     '--gradient-one': 'rgba(52, 88, 128, 1)',
     '--gradient-one-faded': '#516488',
     '--gradient-two': 'rgba(48, 51, 65, 1)',
     '--opacity1': '0.4',
-    '--opacity2': 'none'
+    '--opacity2': 'none',
+    '--tos-border': '#1d2141',
+    '--tos-bg-color': '#A0A0A0',
+    '--test': '#1d2141',
   }
 };
