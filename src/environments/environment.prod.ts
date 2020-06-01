7eae4b476fd80b0a3f151db6e1923bc79eca1d95
Antonij Dzajkoski <djajkoski@its-sk.com.mk>
2020-06-01 19:27:35 +0200

    Fixed env bundle sizes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDiElbCxqoqxJZkU78e-q2WUfX4AzfN9O8',
+    authDomain: 'thesubmarineproject-2c414.firebaseapp.com',
+    databaseURL: 'https://thesubmarineproject-2c414.firebaseio.com/',
+    projectId: 'thesubmarineproject-2c414',
+    storageBucket: 'thesubmarineproject-2c414.appspot.com',
+    messagingSenderId: '995263639995',
+    appId: '1:995263639995:web:c47c7f1fb2c3ae6a98d49e',
+    measurementId: 'G-T3S9QJ4QT5'
+  }
 };
